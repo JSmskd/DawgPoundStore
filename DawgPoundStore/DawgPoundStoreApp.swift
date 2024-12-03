0fc6b345d9421cd32575dd564f3cbeacd8687e8d
JOHN (aka ME) <78168583+JSmskd@users.noreply.github.com>
2024-12-03 11:20:41 -0600

    Initial Commit

---
+++ DawgPoundStore/DawgPoundStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawgPoundStoreApp.swift
+//  DawgPoundStore
+//
+//  Created by John Sencion on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DawgPoundStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
