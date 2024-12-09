466abb9ae42069a2130d4b24ec21bbd9edc907aa
Nicole Park <ypark5275@stu.d214.org>
2024-12-09 10:33:32 -0600

    created Product View

---
+++ DawgPoundStore/ProductView.swift
@@ -0,0 +1,15 @@
+//
+//  ProductView.swift
+//  DawgPoundStore
+//
+//  Created by Yewon Park on 12/9/24.
+//
+
+import SwiftUI
+
+struct ProductView: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
