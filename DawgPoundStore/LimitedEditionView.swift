650c16e191a82de83d2e7bdda5de867a75ddcae1
Zuzanna Lasota <zlasota5196@stu.d214.org>
2024-12-13 10:49:15 -0600

    All views have a simulator

--- DawgPoundStore/LimitedEditionView.swift
+++ DawgPoundStore/LimitedEditionView.swift
@@ -1,1 +1,12 @@
 
+import SwiftUI
+
+struct LimitedEditionView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    LimitedEditionView ()
+}
