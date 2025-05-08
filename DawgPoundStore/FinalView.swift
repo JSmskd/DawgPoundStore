10516341642708787426b3458be19b94cd359284
JohnS <78168583+JSmskd@users.noreply.github.com>
2025-05-08 12:04:05 -0500

    adds order, but not ItemOrder :(

--- DawgPoundStore/FinalView.swift
+++ DawgPoundStore/FinalView.swift
@@ -1,16 +1,36 @@
 import SwiftUI
-
+import CloudKit
 struct FinalView: View {
 //    var model:StateObject<ItemViewModel>
     @EnvironmentObject var model: ItemViewModel
     init (/*_ model:StateObject<ItemViewModel>*/) {
 //        self.model = model
     }
+    @State var fin:Bool = false
     var body: some View {
         Text("Thank you for ordering!")
             .font(.custom("Lexend-Regular", size: 24))
             .padding()
-        
+            .onAppear {
+                var total:[CKRecord.Reference] = []
+                for r in model.cart {
+                    var o:CKRecord = .init(recordType: "orderItem")
+                    o.setObject(r.item.reference!, forKey: "Item")//reference
+                    o.setObject(r.quantity as __CKRecordObjCValue, forKey: "quantity")
+//                    o.setObject(r.blnk.record!.recordID.recordName, forKey: "blankSize")//refrence
+//                    o.setObject("", forKey: "style")//String
+                    let x = CKRecord.Reference.init(record: o, action:.none)
+                    print(x)
+                    total.append(x)
+                    model.database.save(o, completionHandler: { _, _ in
+                    })
+                }
+                var o:CKRecord = .init(recordType: "Order")
+                DispatchQueue.main.schedule {
+                    model.database.save(o, completionHandler: { _, _ in
+                    })
+                }
+            }
         ZStack {
             Rectangle()
                 .foregroundStyle(Color.gray.opacity(0.3))
@@ -22,13 +42,14 @@ struct FinalView: View {
                 .multilineTextAlignment(.center)
                 .padding(.horizontal, 20)
             
-        }
+        }.navigationBarBackButtonHidden()
         
         
         NavigationLink {
             HomeView(/*model*/)
         } label: {
             Text("Return to Home Page")
+                .disabled(fin)
                 .font(Font.custom("Lexend-Regular", size: 24))
                 .foregroundColor(.white)
                 .frame(maxWidth: .infinity)
--- DawgPoundStore/PickUpView.swift
+++ DawgPoundStore/PickUpView.swift
@@ -1,24 +1,15 @@
 import SwiftUI
 
 struct PickUpView: View {
-//    var model:StateObject<ItemViewModel>
+    //    var model:StateObject<ItemViewModel>
     @EnvironmentObject var model: ItemViewModel
     init(){}
-//    init (_ model:StateObject<ItemViewModel>? = nil) {
-//        self.model = model
-//        items = []
-//    }
-//    @State var items:[orderItem]
-//    init (_ model:StateObject<ItemViewModel>? = nil, items: [orderItem]) {
-//        self.model = model
-//        self.items = items
-//    }
     var body: some View {
         @AppStorage("email") var email: String = ""
         @AppStorage("time") var time: String = ""
         ZStack {
             Color.black.edgesIgnoringSafeArea(.all) // Background color
-            
+
             VStack {
                 // Header
                 HStack {
@@ -29,7 +20,7 @@ struct PickUpView: View {
                     Spacer()
                 }
                 .padding()
-                
+
                 VStack {
                     Text("Pick up info")
                         .font(Font.custom("Lexend-Bold", size: 24))
@@ -52,95 +43,96 @@ struct PickUpView: View {
                 .background(Color.white.opacity(0.1))
                 .cornerRadius(10)
                 .padding()
-                
-                
+
+
                 // Cart Items
-                ScrollView {
-                    VStack(spacing: 20) {
-                        HStack {
-                            Image(systemName: "photo")
-                                .resizable()
-                                .frame(width: 80, height: 80)
-                                .background(Color.gray.opacity(0.2))
-                                .cornerRadius(10)
-                            
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Independent Trading Co.")
-                                    .font(.system(size: 10, weight: .light))
-                                    .foregroundColor(.white)
-                                Text("Hersey Hoodie with Husky Head")
-                                    .font(.system(size: 15, weight: .bold))
-                                    .foregroundColor(.white)
-                                Text("Gray")
-                                    .font(.system(size: 10))
-                                    .foregroundColor(.white)
-                                Text("Size S")
-                                    .font(.system(size: 10))
-                                    .foregroundColor(.white)
+                List{
+                    ForEach(_model.projectedValue.cart, id:\.self) { i in
+                        VStack(spacing: 20) {
+                            HStack {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .frame(width: 80, height: 80)
+                                    .background(Color.gray.opacity(0.2))
+                                    .cornerRadius(10)
+
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text(i.wrappedValue.item.title )
+                                        .font(.system(size: 10, weight: .light))
+                                        .foregroundColor(.white)
+                                    Text("Hersey Hoodie with Husky Head")
+                                        .font(.system(size: 15, weight: .bold))
+                                        .foregroundColor(.white)
+                                    Text("Gray")
+                                        .font(.system(size: 10))
+                                        .foregroundColor(.white)
+                                    Text("Size S")
+                                        .font(.system(size: 10))
+                                        .foregroundColor(.white)
+                                }
+                                Spacer()
+
                             }
-                            Spacer()
-                            
-                        }
-                        .padding()
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(10)
-                    }
-                    
-                    // Order Summary
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Order Summary")
-                            .font(Font.custom("Lexend-Bold", size: 24))
-                            .foregroundColor(.white)
-                        
-                        HStack {
-                            Text("Clothing cost (2)")
-                                .font(Font.custom("Lexend-Thin", size: 15))
-                                .foregroundColor(.white)
-                            Spacer()
-                            Text("$90")
-                                .font(Font.custom("Lexend-Thin", size: 15))
-                                .foregroundColor(.white)
-                        }
-                        
-                        HStack {
-                            Text("Maintenance fee")
-                                .font(Font.custom("Lexend-Thin", size: 15))
-                                .foregroundColor(.white)
-                            Spacer()
-                            Text("$8")
-                                .font(Font.custom("Lexend-Thin", size: 15))
-                                .foregroundColor(.white)
-                        }
-                        
-                        Divider()
-                            .background(Color.gray)
-                        
-                        HStack {
-                            Text("Total")
-                                .font(Font.custom("Lexend-Bold", size: 24))
-                                .foregroundColor(.white)
-                            Spacer()
-                            Text("$98.00")
-                                .font(Font.custom("Lexend-Bold", size: 24))
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-                    
-                    NavigationLink(destination: FinalView(/*model*/)) {
-                        Text("Place order")
-                            .font(Font.custom("Lexend-Bold", size: 24))
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
                             .padding()
-                            .background(Color.orange)
+                            .background(Color.gray.opacity(0.2))
                             .cornerRadius(10)
+                        }
                     }
-                    .padding()
                 }
+                // Order Summary
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Order Summary")
+                        .font(Font.custom("Lexend-Bold", size: 24))
+                        .foregroundColor(.white)
+
+                    HStack {
+                        Text("Clothing cost (2)")
+                            .font(Font.custom("Lexend-Thin", size: 15))
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("$90")
+                            .font(Font.custom("Lexend-Thin", size: 15))
+                            .foregroundColor(.white)
+                    }
+
+                    HStack {
+                        Text("Maintenance fee")
+                            .font(Font.custom("Lexend-Thin", size: 15))
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("$8")
+                            .font(Font.custom("Lexend-Thin", size: 15))
+                            .foregroundColor(.white)
+                    }
+
+                    Divider()
+                        .background(Color.gray)
+
+                    HStack {
+                        Text("Total")
+                            .font(Font.custom("Lexend-Bold", size: 24))
+                            .foregroundColor(.white)
+                        Spacer()
+                        Text("$98.00")
+                            .font(Font.custom("Lexend-Bold", size: 24))
+                            .foregroundColor(.white)
+                    }
+                }
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .padding(.horizontal)
+
+                NavigationLink(destination: FinalView(/*model*/)) {
+                    Text("Place order")
+                        .font(Font.custom("Lexend-Bold", size: 24))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.orange)
+                        .cornerRadius(10)
+                }
+                .padding()
             }
         }
     }
